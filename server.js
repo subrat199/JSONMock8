a11a23b7f71961f77580d1320293e7d2b09f6665
subrat199 <subratt01998@gmail.com>
2023-06-03 11:50:59 +0530

    deploy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
-
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/api', router)
+server.use(middlewares);
+server.use("", router);
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
+
+
+
